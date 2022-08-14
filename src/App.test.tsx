c0f085d61176c4b501c95d616fd49e98970f2946
Vincent Farah <vincent.farah@madetech.com>
2022-08-15 00:07:13 +0100

    Add an exaple of consumign a test button ready to branch

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('should render a learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('should render a test button', () => {
+    render(<App />);
+    const linkElement = screen.getByRole('button');
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveTextContent('Test');
+  });
 });
